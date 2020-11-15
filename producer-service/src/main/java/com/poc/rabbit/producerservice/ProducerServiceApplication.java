9f00388051683708c40ce80b5c42f23c0ad6695f
Kanwar Pannu <kanwargurparvezsingh@yahoo.co.in>
2020-11-14 21:02:02 -0500

    Add swagger as root url

--- producer-service/src/main/java/com/poc/rabbit/producerservice/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/poc/rabbit/producerservice/ProducerServiceApplication.java
@@ -3,13 +3,13 @@ package com.poc.rabbit.producerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //TODO: add one more service and have a message from producer directly to it and another going from producer to direct consumer to the new one
-//TODO: add swagger-ui as default link when producer service root url is called
 //TODO: see if anything can be configurable and add to config file
 //TODO: Add Test cases
 //TODO: add test case for logs (look at other logging test cases)
 //TODO: format code and add all log messages
 //TODO: rewrite readme
 //TODO: convert spring boot apps to 1 docker-image so they can run directly from docker-compose
+//TODO: Done :-)
 @SpringBootApplication
 public class ProducerServiceApplication {
 
